774d531d4166bf10712683746b196a68b98dbb8c
Sudheesh <sudheeshpoolakkal@outlook.com>
2024-10-20 15:25:42 +0530

    created a branch for server  and worked here lol

---
+++ server/controllers/adminController.js
@@ -0,0 +1,15 @@
+
+
+
+// API for adding doctor
+const addDoctor = async () => {
+    try{
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.file
+        console.log({ name, email, password, speciality, degree, experience, about, fees, address },imageFile)
+    }catch(error){
+
+    }
+}
+
+export {addDoctor}
---
+++ server/routes/adminRoute.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { addDoctor } from '../controllers/adminController.js';
+import upload from '../middlewares/multer'
+
+const adminRouter = express.Router()
+
+adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
+
+export default adminRouter
