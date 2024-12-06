44b15f1a8bf89308bc2597740dbc5296933cd78b
Dilshad <diluk2005786@gmail.com>
2024-12-06 23:32:46 +0530

    userController and userRoute is partially completed

--- server/index.js
+++ server/index.js
@@ -4,20 +4,24 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
+import userRouter from './routes/userRoute.js'
+
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
+
 //middlewares
 app.use(express.json())
 app.use(cors())
 
 //api endpoints
 app.use('/api/admin',adminRouter)
-// localhost:4000/api/admin
+app.use('/api/user',userRouter)
 
+// localhost:4000/api/admin
 app.get('/',(req,res)=>{
     res.send('API WORKING')
 })
