0f85b148920c02d820afa3bdf770172a8a46a62f
Dilshad <diluk2005786@gmail.com>
2024-12-08 23:41:57 +0530

    userController and userRoute is modified

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -51,5 +51,32 @@ const registerUser = async (req,res)=>{
     }
 }
 
+// API for user Login
+const loginUser = async (req,res) => {
+    try{
 
-export { registerUser }
+        const {email,password} = req.body
+        const user = await userModel.findOne({email})
+
+        if (!user){
+            return res.json({success:false,message:"User does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if (isMatch){
+            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+
+        else{
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+    }
+    catch (error){
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export { registerUser, loginUser }
