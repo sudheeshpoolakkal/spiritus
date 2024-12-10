9f7b741752a411ed86978ec4fe2330ab8c270482
Sreerag <sreerag2114@gmail.com>
2024-12-10 21:30:57 +0530

    Your commit message here

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const DoctorsList = () => {
+
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext)
+  
+  useEffect(()=>{
+     if (aToken){
+      getAllDoctors()
+     }
+
+  },[aToken])
+
+
   return (
-    <div>DoctorsList</div>
+    <div>
+      
+
+    </div>
   )
 }
 
