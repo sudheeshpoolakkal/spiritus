a9809c16c5dcc13289f877388a6517dae91c01db
Sreerag <sreerag2114@gmail.com>
2025-02-02 11:42:14 +0530

    Saving local changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,10 @@ import Dashboard from './pages/Admin/Dashboard';
 import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorsList from './pages/Admin/DoctorsList';
+import { DoctorContext } from './context/DoctorContext';
+import { DoctorDashboard } from './pages/Doctor/DoctorDashboard';
+import DoctorAppointments  from './pages/Doctor/DoctorAppointments';
+import { DoctorProfile } from './pages/Doctor/DoctorProfile';
 
 
 
@@ -17,8 +21,9 @@ import DoctorsList from './pages/Admin/DoctorsList';
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
+  const {dToken} = useContext(DoctorContext)
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
       <Navbar/>
@@ -26,11 +31,20 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar/>
         <Routes>
+          {/*Admin Route*/}
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard/>} />
           <Route path="/all-appointments" element={<AllAppointments/>} />
           <Route path="/add-doctor" element={<AddDoctor/>} />
           <Route path="/doctor-list" element={<DoctorsList/>} />
+
+          {/*Doctor Route*/}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard/>} />
+          <Route path="/doctor-appointments" element={<DoctorAppointments/>} />
+          <Route path="doctor-profile" element={<DoctorProfile/>}/>
+          
+
+
         </Routes>
       </div>
 
---
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DoctorProfile = () => {
+  return (
+    <div>DoctorProfile</div>
+  )
+}
