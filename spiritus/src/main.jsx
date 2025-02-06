b3323177ae3233dca0c08e3f4ff3ecfa1b60dd2c
Sudheesh <sudheeshpoolakkal@outlook.com>
2025-02-06 16:00:44 +0530

    Force pushing local to remote

--- spiritus/src/main.jsx
+++ spiritus/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
-
+//
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <AppContextProvider><App/></AppContextProvider>
