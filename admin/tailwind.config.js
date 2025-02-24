80a8fcec41a49ecbc245b9fc41a87d92eb34d977
Dilshad <diluk2005786@gmail.com>
2025-02-24 20:56:21 +0530

    Admin and Doctor page tailwind css fixed

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,10 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      'primary':"#5f6fff"
+    extend: {
+      colors: {
+        primary: "#5f6fff",
+      },
     },
   },
   plugins: [],
-}
+};
