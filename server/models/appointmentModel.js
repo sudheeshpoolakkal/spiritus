b7f718d735a71feb0f2e5c324505a7e2c5a9ed52
Sreerag <sreerag2114@gmail.com>
2025-03-02 17:34:41 +0530

    solved some bugs

--- server/models/appointmentModel.js
+++ server/models/appointmentModel.js
@@ -14,7 +14,8 @@ const appointmentSchema = new mongoose.Schema({
     isCompleted: { type: Boolean, default: false},
     videoCallLink: String, // Store the video call link
     rating: { type: Number, min: 1, max: 5 }, // Rating given by the user
-    review: { type: String, default: '' } // Optional review
+    review: { type: String, default: '' }, // Optional review
+    patientDescription: { type: String, default: '' } // Adding patient description field
 })
 
 const appointmentModel = mongoose.models.appointment || mongoose.model('appointment',appointmentSchema)
