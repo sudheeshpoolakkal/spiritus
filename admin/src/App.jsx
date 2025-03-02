a801a2137758a7fb0b317726e0c104155f181751
sudheesh <sudheeshpoolakkal@outlook.com>
2025-03-02 21:55:21 +0530

    removed yarn

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,69 +1,58 @@
-import React, { useContext } from 'react'
-import Login from './pages/Login'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { AdminContext } from './context/AdminContext';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Admin/Dashboard';
-import AllAppointments from './pages/Admin/AllAppointments';
-import AddDoctor from './pages/Admin/AddDoctor';
-import DoctorsList from './pages/Admin/DoctorsList';
-import { DoctorContext } from './context/DoctorContext';
-import { DoctorDashboard } from './pages/Doctor/DoctorDashboard';
-import DoctorAppointments  from './pages/Doctor/DoctorAppointments';
-import { DoctorProfile } from './pages/Doctor/DoctorProfile';
-import DoctorPrescription from './pages/Doctor/DoctorPrescription';
-import AdminPrescription from './pages/Admin/AdminPrescription';
-import FeedBack from './pages/Admin/FeedBack';
-
-
-
-
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import { DoctorContext } from "./context/DoctorContext";
+import { DoctorDashboard } from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointments";
+import { DoctorProfile } from "./pages/Doctor/DoctorProfile";
+import DoctorPrescription from "./pages/Doctor/DoctorPrescription";
+import AdminPrescription from "./pages/Admin/AdminPrescription";
+import FeedBack from "./pages/Admin/FeedBack";
 
 const App = () => {
-
-  const {aToken} = useContext(AdminContext)
-  const {dToken} = useContext(DoctorContext)
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
   return aToken || dToken ? (
-    <div className='bg-[#F8F9FD]'>
-      <ToastContainer/>
-      <Navbar/>
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
 
-      <div className='flex items-start'>
-        <Sidebar/>
+      <div className="flex items-start">
+        <Sidebar />
         <Routes>
           {/*Admin Route*/}
           <Route path="/" element={<></>} />
-          <Route path="/admin-dashboard" element={<Dashboard/>} />
-          <Route path="/all-appointments" element={<AllAppointments/>} />
-          <Route path="/add-doctor" element={<AddDoctor/>} />
-          <Route path="/doctor-list" element={<DoctorsList/>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
           <Route path="/admin-prescription" element={<AdminPrescription />} />
           <Route path="/feedback" element={<FeedBack />} />
 
-
-
           {/*Doctor Route*/}
-          <Route path="/doctor-dashboard" element={<DoctorDashboard/>} />
-          <Route path="/doctor-appointments" element={<DoctorAppointments/>} />
-          <Route path="/doctor-profile" element={<DoctorProfile/>}/>
-          <Route path="/doctor-prescription" element={<DoctorPrescription/>}/>
-          
-
-
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointments />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+          <Route path="/doctor-prescription" element={<DoctorPrescription />} />
         </Routes>
       </div>
-
     </div>
   ) : (
     <>
-      <Login/>
-      <ToastContainer/>
+      <Login />
+      <ToastContainer />
     </>
-  )
- }
+  );
+};
 
-export default App
+export default App;
--- spiritus/src/pages/Home.jsx
+++ spiritus/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <>
       <Header />
       <SpecialityMenu />
-      
+
       <TopDoctors />
       <Banner />
 
