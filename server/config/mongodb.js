2c37724a7d29a3e8b9130b11c2ab3aa7495f90cf
Sreerag <sreerag2114@gmail.com>
2025-03-03 12:29:22 +0530

    Removed admin and server folders from remote repository

--- server/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log("Database connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/spiritus`)
-}
-
-export default connectDB
