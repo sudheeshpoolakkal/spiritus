83c7a75521a130e460a7433684ea016dddebf2da
Dilshad <diluk2005786@gmail.com>
2025-03-03 12:02:47 +0530

    image upload successfully

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,15 +1,6 @@
 import multer from 'multer';
-import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Ensure this directory exists
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Save with a unique name
-  },
-});
-
+const storage = multer.memoryStorage(); // Use memory storage instead of disk storage
 const upload = multer({ storage });
 
 export default upload;
