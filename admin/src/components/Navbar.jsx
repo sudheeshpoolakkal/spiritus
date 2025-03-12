d34efcaf7317e3051748f89ec0367ad5c7e961b1
Sudheesh <114869358+sudheeshpoolakkal@users.noreply.github.com>
2025-03-12 18:57:59 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -21,7 +21,23 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
         <div className='flex items-center gap-2 text-xs'>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+            {/* Logo + Text Container with slight negative left margin */}
+        <div className="flex items-center h-5">
+          <img
+            onClick={() => navigate("/")}
+            style={{ transform: "scale(1.7)" }}
+            className="h-full w-auto cursor-pointer object-contain"
+            src={assets.admin_logo}
+            alt="Logo"
+          />
+          <span
+            onClick={() => navigate("/")}
+            style={{ fontFamily: '"Computer Modern", serif' }}
+            className="text-2xl font-medium ml-2 cursor-pointer"
+          >
+            spiritus
+          </span>
+        </div>
             <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
         <button onClick={logout} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
