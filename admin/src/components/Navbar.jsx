f3dca6f74d53c6452af79a2a2d75ae538a0c041c
sudheesh <sudheeshpoolakkal@outlook.com>
2025-03-13 11:38:15 +0530

    logo change

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -22,16 +22,16 @@ const Navbar = () => {
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
         <div className='flex items-center gap-2 text-xs'>
             {/* Logo + Text Container with slight negative left margin */}
-        <div className="flex items-center h-5">
+        <div className="flex items-center h-10">
           <img
-            onClick={() => navigate("/")}
+            onClick={() => navigate("/doctor-dashboard")}
             style={{ transform: "scale(1.7)" }}
             className="h-full w-auto cursor-pointer object-contain"
             src={assets.admin_logo}
             alt="Logo"
           />
           <span
-            onClick={() => navigate("/")}
+            onClick={() => navigate("/doctor-dashboard")}
             style={{ fontFamily: '"Computer Modern", serif' }}
             className="text-2xl font-medium ml-2 cursor-pointer"
           >
